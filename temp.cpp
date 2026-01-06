f4e65d553d1eda118d078bcccdfcafc524b266d4
guptamukund22 <guptamukund22@gmail.com>
2026-01-07 00:10:53 +0530

    DiscountApp added

--- temp.cpp
+++ temp.cpp
@@ -1,26 +1,13 @@
 #include<iostream>
+#include<string>
+#include<sstream>
+#include<map>
+
 using namespace std;
 
-class Base{
-    public:
-        virtual void test(){
-            cout<<"This is the base class"<<'\n';
-        }
-};
-
-class Child : public Base{
-    public:
-
-        void test() override{
-            cout<<"This is the child class"<<'\n';
-        }
-
-        void test2(){
-            cout<<"Just checking"<<'\n';
-        }
-};
-
 int main(){
-    Base * b = new Child();
+    string name = "Mukund";
+    stringstream ss(name);
+    
     return 0;
 }
